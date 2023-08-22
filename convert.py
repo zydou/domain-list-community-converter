325ab8c23d150cb279bf3d36f6ebda2d06f397d8
zydou <git@zydou.me>
2023-08-22 14:57:18 +0800

    feat: add rules statistics

--- convert.py
+++ convert.py
@@ -6,6 +6,19 @@ from collections import defaultdict
 from pathlib import Path
 
 
+def count_lines(path: Path) -> int:
+    """Count lines of a file.
+
+    Args:
+        path (Path): file path
+
+    Returns:
+        int: number of lines
+    """
+    with path.open() as fp:
+        return len(fp.readlines())
+
+
 def parse_category(category: Path, rules: list, attrs: dict) -> tuple[list, dict]:
     """Parse category and return clash rules.
 
@@ -40,10 +53,36 @@ def parse_category(category: Path, rules: list, attrs: dict) -> tuple[list, dict
     return rules, attrs
 
 
-def write_clash(category: str, rules: list, attrs: dict) -> None:
+def save_metainfo(root: Path, num_header_lines: int = 0) -> None:
+    """Save a csv file to display filenames and number of rules.
+
+    Args:
+        root (Path): root dir of rules
+        num_header_lines (int): number of header lines
+    """
+    outpath = root / "README.md"
+    with open(outpath, "w") as f:
+        f.write("| 文件名(Filename) | 规则数(Num rules) | 属性(Attribute) |\n")
+        f.write("|---|---|---|\n")
+        for category in sorted(root.iterdir()):
+            filename = category.name
+            if filename == "README.md":
+                continue
+            num_rules = count_lines(category) - num_header_lines
+            if "@" in filename:
+                filename = filename.replace("@", "@<span></span>")  # add a space to prevent github from parsing it as an email address
+                attr = category.stem.split("@")[-1]
+                f.write(f"| {filename} | {num_rules} | @{attr} |\n")
+            else:
+                attr = ""
+                f.write(f"| {filename} | {num_rules} | {attr} |\n")
+
+
+def write_clash(outroot: Path, category: str, rules: list, attrs: dict) -> None:
     """Write clash rules to file.
 
     Args:
+        outroot (Path): output root dir of rules
         category (str): category name
         rules (list): rules of default
         attrs (dict): rules of attrs
@@ -52,6 +91,7 @@ def write_clash(category: str, rules: list, attrs: dict) -> None:
     def save_to_disk(outpath: Path, rule_lst: list) -> None:
         with outpath.open("w") as f:
             f.write("---\n")
+            f.write(f"# Generated from https://github.com/v2fly/domain-list-community/blob/master/data/{category}\n")
             f.write("payload:\n")
             for rule in rule_lst:
                 if rule.startswith("full:"):
@@ -61,7 +101,6 @@ def write_clash(category: str, rules: list, attrs: dict) -> None:
                     # source: https://lancellc.gitbook.io/clash/clash-config-file/syntax
                     f.write(f"  - '+.{rule}'\n")
 
-    outroot = Path("clash")
     outroot.mkdir(parents=True, exist_ok=True)
     outpath = outroot / f"{category}.yml"
     save_to_disk(outpath, rules)
@@ -69,10 +108,12 @@ def write_clash(category: str, rules: list, attrs: dict) -> None:
         outpath = outroot / f"{category}@{attr}.yml"
         save_to_disk(outpath, rule_list)
 
-def write_surge_domain_set(category: str, rules: list, attrs: dict) -> None:
+
+def write_surge_domain_set(outroot: Path, category: str, rules: list, attrs: dict) -> None:
     """Write surge domain-set rules to file.
 
     Args:
+        outroot (Path): output root dir of rules
         category (str): category name
         rules (list): rules of default
         attrs (dict): rules of attrs
@@ -86,7 +127,6 @@ def write_surge_domain_set(category: str, rules: list, attrs: dict) -> None:
                 else:
                     f.write(f".{rule}\n")
 
-    outroot = Path("surge-domain-set")
     outroot.mkdir(parents=True, exist_ok=True)
     outpath = outroot / f"{category}.txt"
     save_to_disk(outpath, rules)
@@ -94,10 +134,12 @@ def write_surge_domain_set(category: str, rules: list, attrs: dict) -> None:
         outpath = outroot / f"{category}@{attr}.txt"
         save_to_disk(outpath, rule_list)
 
-def write_surge_rule_set(category: str, rules: list, attrs: dict) -> None:
+
+def write_surge_rule_set(outroot: Path, category: str, rules: list, attrs: dict) -> None:
     """Write surge rule-set rules to file.
 
     Args:
+        outroot (Path): output root dir of rules
         category (str): category name
         rules (list): rules of default
         attrs (dict): rules of attrs
@@ -111,7 +153,6 @@ def write_surge_rule_set(category: str, rules: list, attrs: dict) -> None:
                 else:
                     f.write(f"DOMAIN-SUFFIX,{rule}\n")
 
-    outroot = Path("surge-rule-set")
     outroot.mkdir(parents=True, exist_ok=True)
     outpath = outroot / f"{category}.txt"
     save_to_disk(outpath, rules)
@@ -123,14 +164,20 @@ def write_surge_rule_set(category: str, rules: list, attrs: dict) -> None:
 def main() -> None:
     """Entry point."""
     root = Path("domain-list-community/data")
+    clash_root = Path("clash")
+    surge_domain_set_root = Path("surge-domain-set")
+    surge_rule_set_root = Path("surge-rule-set")
     for category in root.iterdir():
         print(category)
         rules = []
         attrs = defaultdict(list)
         rules, attrs = parse_category(category, rules, attrs)
-        write_clash(category.name, rules, attrs)
-        write_surge_domain_set(category.name, rules, attrs)
-        write_surge_rule_set(category.name, rules, attrs)
+        write_clash(clash_root, category.name, rules, attrs)
+        write_surge_domain_set(surge_domain_set_root, category.name, rules, attrs)
+        write_surge_rule_set(surge_rule_set_root, category.name, rules, attrs)
+    save_metainfo(clash_root, num_header_lines=3)
+    save_metainfo(surge_domain_set_root)
+    save_metainfo(surge_rule_set_root)
 
 
 if __name__ == "__main__":
